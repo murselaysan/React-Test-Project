e04e9b6b5a55c67b35fdeddc306d9c83e1c5a451
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-11-11 15:51:44 +0000

    Create first component

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import BuildItem from './BuildItem';
 
 function App() {
   return (
     <div className="App">
-     hello world 
+      <BuildItem />
     </div>
   );
 }
---
+++ test-app/src/BuildItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function BuildItem() {
+    return (
+        <div>
+          <h1> Blog Item Component </h1>  
+          <p>Lorem Ipsum</p>
+        </div>
+    )
+}
