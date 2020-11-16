614302a1f7ed7324f3a6a7cf756e5c2a5bcf3b94
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-11-16 19:59:20 +0000

    Added new State

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,17 +1,24 @@
-
+import React,{useState} from "react";
 import './App.css';
 import Build from './components/BuildItem';
 import BlogForm from './components/BlogForm';
+import Name from "./components/Name";
 
 
 function App() {
+
+  const[name,setName] = useState("mursel");
+
+
   const handleClick = () =>{
         console.log("hello")
+        setName(name)
   }
   return (
     <div className="App">
       <Build/>
       <BlogForm  handleClick = {handleClick}/>
+      <Name  name={name}/>
     </div>
   );
 }
---
+++ test-app/src/components/Name.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Name({name}) {
+    return (
+        <div>
+            <p>My name is {name}</p>
+        </div>
+    )
+}
